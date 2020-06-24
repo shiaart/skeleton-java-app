03308d4d30ac6b8cf227c8321c85281163fdacd7
shiaart <shiabutdinov@gmail.com>
2020-06-24 17:37:30 +0300

    commit3

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -1,5 +1,8 @@
 package com.develogical;
 
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+
 public class QueryProcessor {
 
     public String process(String query) {
@@ -8,9 +11,12 @@ public class QueryProcessor {
         }
         else if (query.toLowerCase().contains("what is your name")) {
             return "Artem";
-        }
-        else if (query.toLowerCase().contains("what is 1 plus 12")) {
-            return "13";
+        }//"what is 1 plus 12"
+        else if (query.toLowerCase().contains("what is ") && query.toLowerCase().contains(" plus ") ) {
+            String[] res = query.toLowerCase().replace("what is ", "").split(" plus ");
+
+
+            return String.valueOf(Integer.valueOf(res[0]) + Integer.valueOf(res[1]));
         }
         else if (query.toLowerCase().contains("which of the following numbers is the largest: 267, 647, 86, 4")) {
             return "647";

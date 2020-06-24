38dedb60674da7dd19a7e82bf04dc537474bc45d
shiaart <shiabutdinov@gmail.com>
2020-06-24 17:44:10 +0300

    commit4

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -2,6 +2,7 @@ package com.develogical;
 
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 public class QueryProcessor {
 
@@ -14,12 +15,21 @@ public class QueryProcessor {
         }//"what is 1 plus 12"
         else if (query.toLowerCase().contains("what is ") && query.toLowerCase().contains(" plus ") ) {
             String[] res = query.toLowerCase().replace("what is ", "").split(" plus ");
-
+            if(res.length == 0) return "";
 
             return String.valueOf(Integer.valueOf(res[0]) + Integer.valueOf(res[1]));
-        }
-        else if (query.toLowerCase().contains("which of the following numbers is the largest: 267, 647, 86, 4")) {
-            return "647";
+        }//which of the following numbers is the largest:
+        else if (query.toLowerCase().contains("which of the following numbers is the largest: ")) {
+            String[] res = query.toLowerCase().replace("which of the following numbers is the largest: ", "").split(",");
+            if(res.length == 0) return "";
+
+            Integer[] arr = new Integer[res.length];
+            for(int i=0;i<res.length;i++){
+                arr[i] = Integer.valueOf(res[i]);
+            }
+            Arrays.sort(arr);
+
+            return String.valueOf(arr[arr.length-1]);
         }
 
 
